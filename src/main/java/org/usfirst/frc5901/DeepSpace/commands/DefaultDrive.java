ca269b4e7ee45ffb53010509217c85bc05129dfc
Team5901 <35078583+Team5901@users.noreply.github.com>
2019-01-25 23:25:45 -0500

    Update DefaultDrive.java

--- src/main/java/org/usfirst/frc5901/DeepSpace/commands/DefaultDrive.java
+++ src/main/java/org/usfirst/frc5901/DeepSpace/commands/DefaultDrive.java
@@ -43,6 +43,7 @@ public class DefaultDrive extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        Robot.drivetrain.drive();
     }
 
     // Make this return true when this Command no longer needs to run execute()

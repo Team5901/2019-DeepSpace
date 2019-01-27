135fde88b17ee90ce26884fbd52a2f498bae7cf9
Team5901 <35078583+Team5901@users.noreply.github.com>
2019-01-26 22:30:34 -0500

    Mal

--- src/main/java/org/usfirst/frc5901/DeepSpace/subsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc5901/DeepSpace/subsystems/Drivetrain.java
@@ -111,6 +111,7 @@ public class Drivetrain extends Subsystem {
 
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new DefaultDrive());
     }
 
     @Override
